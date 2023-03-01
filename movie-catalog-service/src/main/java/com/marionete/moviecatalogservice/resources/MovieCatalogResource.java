61588852b64d36730c74cfc032f6b01b314ad629
Diego Rodrigues <diegorodrigues@diegorodrigues.local>
2023-03-01 11:25:48 +0000

    Movie Info Service calls other services using Service Discovery

--- movie-catalog-service/src/main/java/com/marionete/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/marionete/moviecatalogservice/resources/MovieCatalogResource.java
@@ -26,12 +26,12 @@ public class MovieCatalogResource {
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
          // get all rated movie IDs
-        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+        UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
 
         // for each movie ID, call movie info service and get details
         // put them together
         return ratings.getUserRating().stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
 //            WebClient is an alternative to RestTemplate
 //            Movie movie = webClientBuilder.build()
 //                    .get()
